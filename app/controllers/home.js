667d68b1730bd875ca086f7c6a2f443a7b611842
David Ryan Hall <david.ryan.hall@gmail.com>
2013-12-16 12:36:40 -0800

    First commit with yo express-mysql

---
+++ app/controllers/home.js
@@ -0,0 +1,13 @@
+var db = require('orm').db;
+//  Article = db.models.article;
+
+exports.index = function(req, res){
+  // Article.find(function(err, articles){
+  //   if(err) throw new Error(err);
+  //   res.render('home/index', {
+  //     title: 'Generator-Express MVC',
+  //     articles: articles
+  //   });
+  // });
+  res.render('main/index');
+};
